fdc309029c445c3dccc7bfa5e65afdb3d9383703
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-11-04 15:49:07 -0300

    Iniciando projeto mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,21 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useFonts } from 'expo-font';
+
+import RobotoBold from './assets/fonts/Roboto-Bold.ttf';
+import RobotoRegular from './assets/fonts/Roboto-Regular.ttf';
+import RobotoMedium from './assets/fonts/Roboto-Medium.ttf';
 
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screen/SignIn';
 
 export default function App() {
-  const [fontsLoaded] = useFonts([Roboto_400Regular, Roboto_500Medium, Roboto_700Bold]);
+  const [fontsLoaded] = useFonts({
+    'Roboto_700Bold': RobotoBold,
+    'Roboto_400Regular': RobotoRegular,
+    'Roboto_500Medium': RobotoMedium,
+  });
+  console.log(fontsLoaded);
 
   return (
     <NativeBaseProvider theme={THEME}>
